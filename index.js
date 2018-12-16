9918d972e69746d8c5052acf84cb0b4e8bf71817
piznel <piznel@hotmail.com>
2018-12-16 22:54:32 +0100

    add setup.js file

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ module.exports = function (sails) {
     var install = require('./lib/install.js');
     var exec = require('./lib/exec.js');
     var listen = require('./lib/listen.js');
+    var setup = require('./lib/setup.js');
 
     gladys.on('ready', function(){
         listen();
     });
 
     return {
-        install,
-        exec
+        install: install,
+        setup: setup,
+        exec: exec,
     };
 };
