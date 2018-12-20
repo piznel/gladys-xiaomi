9c58367ebd67784ce17433dcd0e2b39d3f9d0a6d
piznel <piznel@hotmail.com>
2018-12-20 11:24:50 +0100

    Add message invalid key at config module

--- assets/js/xiaomi.controller.js
+++ assets/js/xiaomi.controller.js
@@ -1,88 +1,87 @@
-   (function() {
-     'use strict';
+(function() {
+  'use strict';
 
-     angular
-       .module('gladys')
-       .controller('xiaomiCtrl', xiaomiCtrl);
+  angular
+    .module('gladys')
+    .controller('xiaomiCtrl', xiaomiCtrl);
 
-     xiaomiCtrl.$inject = ['xiaomiService', '$scope'];
+  xiaomiCtrl.$inject = ['xiaomiService', '$scope'];
 
-     function xiaomiCtrl(xiaomiService, $scope) {
-       /* jshint validthis: true */
-       var vm = this
-       vm.gateways = [];
-       vm.gatewaysReady = false;
-       vm.gatewaysCount = 0;
-       vm.uncreated = [];
-       vm.uncreatedReady = false;
-       vm.uncreatedCount = 0;
-       vm.unknown = []
-       vm.unknownReady = false;
-       vm.unknowCount = 0;
+  function xiaomiCtrl(xiaomiService, $scope) {
+    /* jshint validthis: true */
+    var vm = this
+    vm.gateways = [];
+    vm.gatewaysReady = false;
+    vm.gatewaysCount = 0;
+    vm.uncreated = [];
+    vm.uncreatedReady = false;
+    vm.uncreatedCount = 0;
+    vm.unknown = []
+    vm.unknownReady = false;
+    vm.unknowCount = 0;
 
-       vm.refresh = refresh;
-       vm.savePassword = savePassword;
+    vm.refresh = refresh;
+    vm.savePassword = savePassword;
 
-       activate()
+    activate()
 
-       function refresh() {
-         activate()
-       }
+    function refresh() {
+      activate()
+    }
 
-       function savePassword() {
-         return xiaomiService.savePassword(vm.gateways)
-           .then(function(result) {
-             if (result.status == 200) {
-               xiaomiService.successNotificationTranslated('PASSWORD_UPDATED');
-             } else { xiaomiService.errorNotificationTranslated('ERROR') }
-           })
-       }
-
-
-       function activate() {
-         xiaomiService.gateways()
-           .then(function(result) {
-             if (result.status != 200) xiaomiService.errorNotificationTranslated('ERROR')
-             vm.gateways = result.data
-             vm.gatewaysCount = vm.gateways.length
-             if (vm.gatewaysCount === 0) {
-               xiaomiService.errorNotificationTranslated('NO_GATEWAY')
-               vm.gatewaysReady = false
-             } else {
-               vm.gateways.forEach(gateway => {
-                 if(!gateway.validKey) {
-                  xiaomiService.errorNotificationTranslated('INVALID_KEY')
-                  break;
-                 }
-               });
-               if(vm.gateways.k)
-               vm.gatewaysReady = true
-             }
-           })
-
-         xiaomiService.uncreated()
-           .then(function(result) {
-             if (result.status != 200) xiaomiService.errorNotificationTranslated('ERROR')
-             vm.uncreated = result.data
-             vm.uncreatedCount = vm.uncreated.length;
-             if (vm.uncreatedCount === 0) {
-               xiaomiService.successNotificationTranslated('NO_UNKNOWN')
-             }
-             vm.uncreatedReady = true
-           })
-
-         xiaomiService.unknown()
-           .then(function(result) {
-             if (result.status != 200) xiaomiService.errorNotificationTranslated('ERROR')
-             vm.unknown = result.data
-             vm.unknownCount = vm.unknown.length
-             if (vm.unknownCount === 0) {
-               xiaomiService.successNotificationTranslated('NO_UNCREATED')
-             }
-             vm.unknownReady = true
-           })
-       }
+    function savePassword() {
+      return xiaomiService.savePassword(vm.gateways)
+        .then(function(result) {
+          if (result.status == 200) {
+            xiaomiService.successNotificationTranslated('PASSWORD_UPDATED');
+          } else { xiaomiService.errorNotificationTranslated('ERROR') }
+        })
+    }
 
 
-     }
-   })();
+    function activate() {
+      xiaomiService.gateways()
+        .then(function(result) {
+          if (result.status != 200) xiaomiService.errorNotificationTranslated('ERROR')
+          vm.gateways = result.data
+          vm.gatewaysCount = vm.gateways.length
+          if (vm.gatewaysCount === 0) {
+            xiaomiService.errorNotificationTranslated('NO_GATEWAY')
+            vm.gatewaysReady = false
+          } else {
+            for(var gateway of vm.gateways) {
+              if(!gateway.validKey) {
+               xiaomiService.errorNotificationTranslated('INVALID_KEY')
+               break;
+              }
+            };
+            vm.gatewaysReady = true
+          }
+        })
+
+      xiaomiService.uncreated()
+        .then(function(result) {
+          if (result.status != 200) xiaomiService.errorNotificationTranslated('ERROR')
+          vm.uncreated = result.data
+          vm.uncreatedCount = vm.uncreated.length;
+          if (vm.uncreatedCount === 0) {
+            xiaomiService.successNotificationTranslated('NO_UNKNOWN')
+          }
+          vm.uncreatedReady = true
+        })
+
+      xiaomiService.unknown()
+        .then(function(result) {
+          if (result.status != 200) xiaomiService.errorNotificationTranslated('ERROR')
+          vm.unknown = result.data
+          vm.unknownCount = vm.unknown.length
+          if (vm.unknownCount === 0) {
+            xiaomiService.successNotificationTranslated('NO_UNCREATED')
+          }
+          vm.unknownReady = true
+        })
+    }
+
+
+  }
+})();
